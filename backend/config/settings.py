eb90809a917a514d639450067722d24578bfe15e
kk0055 <rtsw.lgy@gmail.com>
2025-07-18 10:08:11 +0900

    s

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,7 +97,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL:
     DATABASES = {
         "default": dj_database_url.parse(
-            DATABASE_URL, conn_max_age=600, ssl_require=True
+            DATABASE_URL, conn_max_age=600
         )
     }
 else:
