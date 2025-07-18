fe03db079da35148d848ccd964cd4b8fe56c905a
kk0055 <rtsw.lgy@gmail.com>
2025-07-18 09:19:45 +0900

    DATABASE_URL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -91,11 +91,14 @@ WSGI_APPLICATION = "config.wsgi.app"
 
 DATABASES = {}
 
-# Vercel環境（本番環境）かどうかを判定し、Vercel Postgresに接続
-if os.getenv("VERCEL_ENV") == "production":
-    # Vercelが自動で設定したDATABASE_URL（またはPOSTGRES_URL）を読み込む
-    db_url = os.getenv("DATABASE_URL", os.getenv("POSTGRES_URL"))
-    DATABASES["default"] = dj_database_url.parse(db_url)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL, conn_max_age=600, ssl_require=True 
+        )
+    }
 else:
 
     DATABASES["default"] = {
